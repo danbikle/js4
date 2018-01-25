dcf026a01efaea49b7f800a7c51610ff249a0692
Dan Bikle <danbikle@outlook.com>
2018-01-25 06:39:53 -0800

    more

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  get  'home/index'
-  root 'home#index'
+  get 'cclasses/index'
 
+  root 'home#about'
+  get  '/about',   to: 'home#about'
+  get  '/blog',    to: 'home#blog'
+  get  '/contact', to: 'home#contact'
+  get ':controller(/:action)'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
